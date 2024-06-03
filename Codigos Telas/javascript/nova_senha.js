5216d1ffa1d374bc0fdfa2b3a2a5a41d1747f556
Ríchard Valandro <118376541+RichardValandro04@users.noreply.github.com>
2024-06-03 19:03:22 -0300

    Delete Codigos Telas directory

--- Codigos Telas/javascript/nova_senha.js
+++
@@ -1,6 +0,0 @@
-//capturando botao de alterar senha
-const botao_alterar = document.querySelector(".botao_alterar");
-
-//adicionando evento de click para exibir um alert de senha alterada
-// obs.: a lógica para validação de senha ainda deverá ser aplicada
-botao_alterar.addEventListener("click", () => { alert("Senha Alterada"); window.location.href = "/index.html"});
